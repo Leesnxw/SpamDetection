85a3d523cd2f3a7149b4c641f469c749c1d259ff
26902 <2690233316@qq.com>
2022-04-01 23:27:42 +0800

    change view_pkl.py

--- test/viewPkl.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-# 预览pickle文件内容
-print(pickle.load(open('.\\data\\sms_data.pickle', 'rb')))
-print(pickle.load(open('.\\data\\sms_data_small.pickle', 'rb')))
---
+++ test/view_pkl.py
@@ -0,0 +1,5 @@
+import pickle
+
+# 预览pickle文件内容
+print(pickle.load(open('d:\\GraduationDesign\\SpamDetection\\data\\sms_data.pickle', 'rb')))
+print(pickle.load(open('d:\\GraduationDesign\\SpamDetection\\data\\sms_data_small.pickle', 'rb')))
